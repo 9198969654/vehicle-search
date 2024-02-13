dee1c5010b2b7fb4f9a68b162a46c0a394e5fb7e
User <narendrakumarnishad56@gmail.com>
2024-02-14 00:23:19 +0530

    Error Handler Crested for No Manufacturer foundh

--- src/main/java/com/learn2code_vehicle_api/vehicle_search/exception/ManufacturerNotFoundException.java
+++ src/main/java/com/learn2code_vehicle_api/vehicle_search/exception/ManufacturerNotFoundException.java
@@ -1,5 +1,6 @@
 package com.learn2code_vehicle_api.vehicle_search.exception;
 
+////////////////nks////
 public class ManufacturerNotFoundException extends Exception{
 
     public ManufacturerNotFoundException(String message){
