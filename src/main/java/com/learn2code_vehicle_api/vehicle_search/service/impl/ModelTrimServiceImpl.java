c706269a3b21a419faa9825e4ffdbbaea8ec76a7
User <narendrakumarnishad56@gmail.com>
2024-03-05 00:29:59 +0530

    created endpoint Mapping concept in manufacturing

--- src/main/java/com/learn2code_vehicle_api/vehicle_search/service/impl/ModelTrimServiceImpl.java
+++ src/main/java/com/learn2code_vehicle_api/vehicle_search/service/impl/ModelTrimServiceImpl.java
@@ -1,7 +1,9 @@
 package com.learn2code_vehicle_api.vehicle_search.service.impl;
 
 import com.learn2code_vehicle_api.vehicle_search.dao.ModelDAO;
+import com.learn2code_vehicle_api.vehicle_search.dao.TrimTypeDAO;
 import com.learn2code_vehicle_api.vehicle_search.entity.Model;
+import com.learn2code_vehicle_api.vehicle_search.entity.TrimType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,33 @@ public class ModelTrimServiceImpl implements ModelTrimService{
     @Autowired
     private ModelDAO modelDAO;
 
+    @Autowired
+    private TrimTypeDAO trimTypeDAO;
+
     @Override
     public Model saveModel(Model model) {
        Model savedModel =  modelDAO.save(model);
         return savedModel;
     }
+
+    @Override
+    public TrimType saveTrimType(TrimType trimType) {
+        return trimTypeDAO.save(trimType);
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
