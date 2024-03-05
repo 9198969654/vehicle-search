95c7b127e181c66d44b690559c763b36fe58a52b
User <narendrakumarnishad56@gmail.com>
2024-03-05 13:50:44 +0530

    add get All model endpoint

--- src/main/java/com/learn2code_vehicle_api/vehicle_search/controller/ModelTrimController.java
+++ src/main/java/com/learn2code_vehicle_api/vehicle_search/controller/ModelTrimController.java
@@ -6,10 +6,9 @@ import com.learn2code_vehicle_api.vehicle_search.service.impl.ModelTrimService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/model-trim")
@@ -28,6 +27,15 @@ public class ModelTrimController {
     public ResponseEntity<TrimType> createTrimType(@RequestBody  TrimType trimType){
       TrimType savedTrim =   modelTrimService.saveTrimType(trimType);
       return ResponseEntity.status(HttpStatus.CREATED).body(savedTrim);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Model>> fetchAllModels(){
+        List<Model> dbModels = modelTrimService.getAllModels();
+        if (dbModels.size()>0){
+            return new ResponseEntity<>(dbModels, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
 }
--- src/main/java/com/learn2code_vehicle_api/vehicle_search/service/impl/ModelTrimService.java
+++ src/main/java/com/learn2code_vehicle_api/vehicle_search/service/impl/ModelTrimService.java
@@ -3,8 +3,11 @@ package com.learn2code_vehicle_api.vehicle_search.service.impl;
 import com.learn2code_vehicle_api.vehicle_search.entity.Model;
 import com.learn2code_vehicle_api.vehicle_search.entity.TrimType;
 
+import java.util.List;
+
 public interface ModelTrimService {
 
     Model saveModel(Model model);
     TrimType saveTrimType(TrimType trimType);
+    List<Model> getAllModels();
 }
