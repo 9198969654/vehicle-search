8642df76c0595b834d046e32fe0d4dacec0d2595
User <narendrakumarnishad56@gmail.com>
2024-03-10 12:38:02 +0530

    Create TrimTypeNotFoundException

---
+++ src/main/java/com/learn2code_vehicle_api/vehicle_search/exception/TrimTypeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.learn2code_vehicle_api.vehicle_search.exception;
+
+public class TrimTypeNotFoundException extends Exception{
+
+    public TrimTypeNotFoundException(String s){
+        super(s);
+    }
+}
--- src/main/java/com/learn2code_vehicle_api/vehicle_search/service/impl/ModelTrimService.java
+++ src/main/java/com/learn2code_vehicle_api/vehicle_search/service/impl/ModelTrimService.java
@@ -3,6 +3,7 @@ package com.learn2code_vehicle_api.vehicle_search.service.impl;
 import com.learn2code_vehicle_api.vehicle_search.entity.Model;
 import com.learn2code_vehicle_api.vehicle_search.entity.TrimType;
 import com.learn2code_vehicle_api.vehicle_search.exception.ModelNotFoundException;
+import com.learn2code_vehicle_api.vehicle_search.exception.TrimTypeNotFoundException;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ public interface ModelTrimService {
     TrimType saveTrimType(TrimType trimType);
     List<Model> getAllModels();
     Model modifyModel(int id,Model model) throws ModelNotFoundException;
-    TrimType modifyTrimType(int id, TrimType trimType);
+    TrimType modifyTrimType(int id, TrimType trimType) throws TrimTypeNotFoundException;
     Model getModelById(int id) throws ModelNotFoundException;
     TrimType getTrimTypeById(int id);
 }
